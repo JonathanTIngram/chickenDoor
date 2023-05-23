76667d37e75e78e7bd2c3e53fd90e93d74e39e0e
JonathanTIngram <38962360+JonathanTIngram@users.noreply.github.com>
2023-05-22 22:59:48 -0700

    Added Open Close times

--- website/index.js
+++ website/index.js
@@ -1,83 +1,105 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const { exec } = require("child_process");
+const path = require('path');
+const fs = require('fs');
 
+let isOpen = null;
+let openingTime = null;
+let closingTime = null;
+
+fs.readFile('isOpen.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  isOpen = data;
+  console.log(isOpen);
+});
 
 app.use(express.static("client"));
-const path = require('path');
-const fs = require('fs')
 
-var isOpen = null;
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
-fs.readFile('isOpen.txt', 'utf8' , (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  isOpen = data
-  console.log(isOpen)
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/index.html'));
+});
 
+app.post('/setTimes', (req, res) => {
+  openingTime = req.body.openingTime;
+  closingTime = req.body.closingTime;
+  console.log(`Opening Time: ${openingTime}`);
+  console.log(`Closing Time: ${closingTime}`);
+  // Write opening time to "openingTime.txt"
+  fs.writeFile('openingTime.txt', openingTime, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Opening time written to openingTime.txt');
+  });
+  // Write closing time to "closingTime.txt"
+  fs.writeFile('closingTime.txt', closingTime, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Closing time written to closingTime.txt');
+  });
+  res.redirect('/');
+});
 
 app.get('/openDoor', (req, res) => {
-
-	console.log(isOpen)
-	if (isOpen == 0) {
-
-    		exec("python3 pythonScripts/openDoor.py", (error, stdout, stderr) => {
-    			if (error) {
-        			console.log(`error: ${error.message}`);
-        			return;
-    			}
-    			if (stderr) {
-        			console.log(`stderr: ${stderr}`);
-        			return;
-    			}
-    			console.log(`stdout: ${stdout}`);
-    		});
-		isOpen = 1;
-		fs.writeFile('isOpen.txt', isOpen.toString(), err => {
-  			if (err) {
-    				console.error(err)
-    				return
-  			}
-			res.sendFile(path.join(__dirname, 'client/open.html'))
-  		//file written successfully
-		})
-   	}
-
+  console.log(isOpen);
+  if (isOpen == 0) {
+    exec("python3 pythonScripts/openDoor.py", (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+    isOpen = 1;
+    fs.writeFile('isOpen.txt', isOpen.toString(), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.sendFile(path.join(__dirname, 'client/open.html'));
+    });
+  }
 });
 
 app.get('/closeDoor', (req, res) => {
-
-	console.log(isOpen)
-	if (isOpen == 1) {
-		exec("python3 pythonScripts/closeDoor.py", (error, stdout, stderr) => {
-    			if (error) {
-        			console.log(`error: ${error.message}`);
-        			return;
-    			}
-    			if (stderr) {
-        			console.log(`stderr: ${stderr}`);
-        			return;
-    			}
-    			console.log(`stdout: ${stdout}`);
-		});
-		isOpen = 0;
-		fs.writeFile('isOpen.txt', isOpen.toString(), err => {
-  			if (err) {
-    				console.error(err)
-    				return
-  			}
-			res.sendFile(path.join(__dirname, 'client/close.html'))
-			
-  		//file written successfully
-		})
-	}
-	
+  console.log(isOpen);
+  if (isOpen == 1) {
+    exec("python3 pythonScripts/closeDoor.py", (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+    isOpen = 0;
+    fs.writeFile('isOpen.txt', isOpen.toString(), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.sendFile(path.join(__dirname, 'client/close.html'));
+    });
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
